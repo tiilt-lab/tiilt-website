3662a5a72781a4717ac1dc6687020cd456cf226b
Your Name <marcjiang312@gmail.com>
2020-04-02 19:20:58 -0500

    Made more changes. Discovered that hover features for fly-out expansion doesn't seem really intuitive.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function showPicture(e) {
+// These functions were made for hovering, which is not included right now
+function onlyShow(e) { 
+    e.className = "lazy"
+}
+
+function onlyHide(e) { 
+    e.className = "visually_hidden"
+}
+
+// These functions were renamed so that purpose will be more clear
+function toggleImage(e) {
     e.classList.toggle("visually_hidden");
 }
 
@@ -34,20 +44,63 @@ function addReadMoreButtons() {
     });
 }
 
-function hideImage(e) {
+function toggleHelper(e, f) {
     const spanElement = e.querySelector('span');
     rotate(spanElement);
 
     const imgElements = e.getElementsByTagName('img');
     const imgElementsInArray = Array.from(imgElements);
-    imgElementsInArray.map((imgTag) => showPicture(imgTag));
+    imgElementsInArray.map((imgTag) => f(imgTag));
 }
 
 window.onload = () => {
+    var timer = null;
     if (location.href.includes("projects")) {
         const projectBoxes = document.getElementsByClassName('project_box');
         const projectBoxesInArray = Array.from(projectBoxes);
-        projectBoxesInArray.map((box) => box.addEventListener('click', () => hideImage(box)));
+        projectBoxesInArray.map((box) => box.addEventListener('click', function () { 
+            toggleHelper(box, toggleImage); 
+            if (box.getAttribute('aria-expanded') == "true") { 
+                box.setAttribute('aria-expanded', "false")
+            } else { 
+                box.setAttribute('aria-expanded', "true")
+            } 
+        }));
+        projectBoxesInArray.map((box) => box.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+              toggleHelper(box, toggleImage)
+              if (box.getAttribute('aria-expanded') == "true") { 
+                  box.setAttribute('aria-expanded', "false")
+              } else { 
+                  box.setAttribute('aria-expanded', "true")
+              }
+            }
+        }));
+
+        // Hover feature doesn't really seem intuitive or make sense upon further inspection
+
+        // projectBoxesInArray.map(function(box) { 
+        //     var spanElement = box.querySelector('span') 
+        //     if (spanElement != null) {  
+        //         // box.addEventListener("mouseover",
+                   // Replace below line with above line to make the hovering on the box rather than the arrow
+        //         spanElement.addEventListener("mouseover", 
+        //             function() { 
+        //                 toggleHelper(box, onlyShow); 
+        //                 clearTimeout(timer); 
+        //             }
+        //         ); 
+        //     } 
+        // })
+        // projectBoxesInArray.map(function(box) { 
+        //     var spanElement = box.querySelector('span') 
+        //     if (spanElement != null) { 
+        //         // box.addEventListener("mouseover",
+                    // Replace below line with above line to make the hovering on the box rather than the arrow
+        //         spanElement.addEventListener("mouseout", 
+        //             () => timer = setTimeout(() => toggleHelper(box, onlyHide), 1000));
+        //     }
+        // })
     }
 
     if (location.href.includes("people")) {
